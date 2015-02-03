52e3da9c1e6c2532aa99ccc38a29510ca568f6f0
Jim <jim.lemmers@gmail.com>
2015-02-03 16:57:31 +0000

    added miner with test functionality

---
+++ survival/miner.js
@@ -0,0 +1,18 @@
+/* 
+ * Module used to command the miners.
+ */
+
+module.exports = function (creep){
+	var target = Memory.resources[creep.memory.source_id].pos;
+	
+	var source = Game.lookAt(target);
+	Game.getUsedCpu(function (cpu){
+		console.log('cpu used during lookat ' + cpu);
+	});
+
+	var source = creep.room.find(Game.SOURCES);
+	
+	Game.getUsedCpu(function (cpu){
+		console.log('cpu used during find' + cpu);
+	});
+}
