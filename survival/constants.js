8391b755a7bad27e57f850747f9fec0ec2fdca6a
Jim <jim.lemmers@gmail.com>
2015-02-08 00:07:16 +0000

    made constants into object with id, body

--- survival/constants.js
+++ survival/constants.js
@@ -3,8 +3,8 @@
  */
 
 module.exports = {
-    "MINER" : 0,
-    "CARRIER" : 1,
+    "MINER" : {'id': 0, 'body': Game.WORK},
+    "CARRIER" : {'id': 1, 'body' : Game.CARRY},
     "BUILDER" : 2,
     "RANGER" : 3,
 }
