523e166991dc3bb1513ab5b52c339f56dc158451
Jim <jim.lemmers@gmail.com>
2015-02-19 19:56:36 +0000

    miner changes i guess

--- survival/miner.js
+++ survival/miner.js
@@ -8,18 +8,21 @@ module.exports = function (creep){
 	var x = mem_source.pos.x;
 	var y = mem_source.pos.y;
 	
-	var source = creep.room.lookAt(x, y)[0].source;
+	var source = Game.getObjectById(creep.memory.source_id);
 	if (!creep.pos.isNearTo(source)){
 		creep.moveTo(source.pos);
 	}
 	else{
 		creep.harvest(source);
 		var carriers = mem_source[C.CARRIER.id];
-		for (var i=0; i < carriers.length; i++){
-			var carrier = Game.creeps[carriers[i]];
-			if (carrier.pos.isNearTo(creep)){
-				creep.transferEnergy(carrier);
-			}
+		if(carriers.length > 0){
+			carriers.forEach(function(carrier){
+				carrier = Game.creeps[carrier];
+				console.log(carrier);
+				if (carrier.pos.isNearTo(creep)){
+					creep.transferEnergy(carrier);
+				}
+			});
 		}
 	}
 }
